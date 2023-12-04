779ba07c466f2cf6f58374cec68b6c5418d1360d
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-04 16:55:52 +0200

    add methods to be implemented

--- City.java
+++ City.java
@@ -1,3 +1,44 @@
-public class City implements CityInterface {
+public class City implements CityInterface, Comparable<City> {
 
+    private int id;
+    private String name;
+    private int population;
+    private int influenzaCases;
+
+    public int getID() {
+        return 0;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public int getPopulation() {
+        return 0;
+    }
+
+    public int getInfluenzaCases() {
+        return 0;
+    }
+
+    public void setID(int ID) {
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public void setPopulation(int population) {
+
+    }
+
+    public void setInfluenzaCases(int InfluenzaCases) {
+
+    }
+
+    @Override
+    public int compareTo(City o) {
+        return 0;
+    }
 }
