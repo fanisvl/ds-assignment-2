fa680f5aae232c3fec6f4ad812b0aea9d4473399
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-04 17:34:18 +0200

    read file to get info for city, create city, save city to arraylist

--- Influenza_k.java
+++ Influenza_k.java
@@ -1,16 +1,35 @@
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Influenza_k {
     public static void main(String[] args) {
+
+        // Get file name
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter daily report filename: ");
         String filename = scanner.nextLine();
 
+        // Create arrayList to store cities
+        ArrayList<City> cities = new ArrayList<>();
+
+        // Read file
         try (Scanner fileReader = new Scanner(Paths.get(filename))) {
             while (fileReader.hasNextLine()) {
-                String cityRow = fileReader.nextLine();
-                System.out.println(cityRow);
+                // Split line to city info.
+                String line = fileReader.nextLine();
+                String[] cityInfo = line.split(" ");
+
+                // Get city info
+                int id = Integer.parseInt(cityInfo[0]);
+                String name = cityInfo[1];
+                int population = Integer.parseInt(cityInfo[2]);
+                int influenzaCases = Integer.parseInt(cityInfo[3]);
+
+                // Create and save city
+                City city = new City(id, name, population, influenzaCases);
+                cities.add(city);
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
