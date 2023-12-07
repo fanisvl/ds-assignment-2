fcec06d38b273f5eef3230998808394fc96781b6
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-07 11:38:37 +0200

    adjust for zero based indexing

--- MaxPQ.java
+++ MaxPQ.java
@@ -3,7 +3,7 @@ public class MaxPQ<Key extends Comparable<Key>> {
     private int N;
 
     public MaxPQ(int capacity) {
-        pq = (Key[]) new Comparable[capacity+1];
+        pq = (Key[]) new Comparable[capacity];
     }
 
     public boolean isEmpty() {
@@ -24,20 +24,16 @@ public class MaxPQ<Key extends Comparable<Key>> {
     }
 
     private void swim(int k) {
-        // parent of k is at k/2
-        while (k > 1 && less(k/2, k)) {
-            exchange(k, k/2);
-            k = k/2;
+        while (k > 0 && less((k - 1) / 2, k)) {
+            exchange(k, (k - 1) / 2);
+            k = (k - 1) / 2;
         }
     }
 
     private void sink(int k) {
-        // 2k and 2k+1 are children of parent node k
-        while (2*k <= N) {
-            int j = 2*k;
-            // If true, 2k+1 is largest child (j++)
-            if (j < N && less(j, j+1)) j++;
-            // If k is larger than j (child), sinking is finished.
+        while (2 * k + 1 < N) {
+            int j = 2 * k + 1;
+            if (j + 1 < N && less(j, j + 1)) j++;
             if (!less(k, j)) break;
             exchange(k, j);
             k = j;
@@ -53,5 +49,6 @@ public class MaxPQ<Key extends Comparable<Key>> {
         pq[i] = pq[j];
         pq[j] = temp;
     }
+
 
 }
